a9b51fc96472a829f501e5623a6db08decaf5808
Mirko Gutierrez <mirko.gutierrez@appx.cl>
2026-01-22 18:15:20 -0300

    actualiza variables de entoerno para local y corrige deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
+#!/bin/bash
+
+# 1. Bajar la última versión de Docker Hub
 sudo docker pull mirkogutierrezappx/auth:latest
 
+# 2. Limpieza de contenedores anteriores
 sudo docker stop auth-container 2>/dev/null
 sudo docker rm auth-container 2>/dev/null
 
-
+# 3. Ejecución corregida
 sudo docker run \
            --restart always \
            -d -p 8083:8083 \
            --env-file .env \
            --network appx \
            --add-host=host.docker.internal:host-gateway \
-           --name auth-container auth \
+           --name auth-container \
            mirkogutierrezappx/auth:latest
-
