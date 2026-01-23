d37a268988f256c6742138af8b43c6459b621304
Mirko Gutierrez <mirko.gutierrez@appx.cl>
2026-01-22 22:32:35 -0300

    cambios en deploy.sh mejroas

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # 1. Bajar la última versión de Docker Hub
-sudo docker pull mirkogutierrezappx/auth:latest
-
+docker pull mirkogutierrezappx/auth:latest
 # 2. Limpieza de contenedores anteriores
-sudo docker stop auth-container 2>/dev/null
-sudo docker rm auth-container 2>/dev/null
+docker stop auth-container 2>/dev/null
+docker rm auth-container 2>/dev/null
 
 # 3. Ejecución corregida
-sudo docker run \
+docker run \
            --restart always \
            -d -p 8083:8083 \
            --env-file .env \
@@ -16,3 +15,6 @@ sudo docker run \
            --add-host=host.docker.internal:host-gateway \
            --name auth-container \
            mirkogutierrezappx/auth:latest
+
+# Borra imágenes antiguas que ya no se usan
+docker image prune -f
